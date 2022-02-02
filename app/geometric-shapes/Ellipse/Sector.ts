e9fc0e4a32804ca1d913e63ec1923ebeb51f06c7
skalem <pimako2003@yandex.by>
2022-02-02 14:22:32 +0300

    Made constructor and implemented basic methods

---
+++ app/geometric-shapes/Ellipse/Sector.ts
@@ -0,0 +1,44 @@
+import { Circle } from "./Circle.js"
+import { Shape } from "../Shape"
+import { Point } from "../Point.js"
+
+
+class Sector extends Shape {
+
+    private radius_;
+    private angleToX_;
+    private angle_;
+
+    protected constructor(shapeId: number, radius: number, angleToX: number, angle: number) {
+        super(shapeId, [new Point(0,0)]);
+        this.radius_ = radius;
+        this.angle_ = angle;
+        this.angleToX_ = angleToX;
+    }
+
+    public upperPointY(): number {
+        throw new Error("Method not implemented.")
+    }
+    public leftPointX(): number {
+        throw new Error("Method not implemented.")
+    }
+    public lowerPointY(): number {
+        throw new Error("Method not implemented.")
+    }
+    public rightPointX(): number {
+        throw new Error("Method not implemented.")
+    }
+    public contains(x: number, y: number): boolean {
+        throw new Error("Method not implemented.")
+    }
+    public draw(context: CanvasRenderingContext2D) {
+        throw new Error("Method not implemented.")
+    }
+    public getPointsForUpdateStatus(): Point[] {
+        throw new Error("Method not implemented.")
+    }
+
+}
+
+export {Sector}
+
